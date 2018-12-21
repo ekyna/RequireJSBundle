3c491a705d5a7baa2ff8ff56f6e939d7a1a52414
Etienne Dauvergne <contact@ekyna.com>
2018-12-21 16:45:40 +0100

    [RequireJs] Removed bower.json. Added package.json. Updated js files.

--- Resources/config/grunt/uglify.js
+++ Resources/config/grunt/uglify.js
@@ -3,19 +3,16 @@ module.exports = function (grunt, options) {
         require: {
             files: {
                 'src/Ekyna/Bundle/RequireJsBundle/Resources/public/plugin/domReady.js': [
-                    'bower_components/domReady/domReady.js'
+                    'node_modules/requirejs-domready/domReady.js'
                 ],
                 'src/Ekyna/Bundle/RequireJsBundle/Resources/public/plugin/json.js': [
-                    'bower_components/requirejs-plugins/src/json.js'
+                    'node_modules/requirejs-plugins/src/json.js'
                 ],
                 'src/Ekyna/Bundle/RequireJsBundle/Resources/public/plugin/text.js': [
-                    'bower_components/text/text.js'
+                    'node_modules/requirejs-text/text.js'
                 ],
                 'src/Ekyna/Bundle/RequireJsBundle/Resources/public/require.js': [
                     'node_modules/requirejs/require.js'
-                ],
-                'src/Ekyna/Bundle/RequireJsBundle/Resources/public/r.js': [
-                    'node_modules/requirejs/bin/r.js'
                 ]
             }
         }
